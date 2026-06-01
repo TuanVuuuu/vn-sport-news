b3160bf8f49da027db538ed78105dc26b4f90b6b
TuanVu <vunt@vnpt-technology.vn>
2026-06-01 22:19:16 +0700

    Thêm categories

--- src/config/categories.js
+++ src/config/categories.js
@@ -22,6 +22,86 @@ const categories = [
         name: 'Thể thao',
         rssUrl: 'https://vnexpress.net/rss/the-thao.rss',
     },
+    {
+        id: 'world',
+        name: 'Thế giới',
+        rssUrl: 'https://vnexpress.net/rss/the-gioi.rss',
+    },
+    {
+        id: 'relax',
+        name: 'Thư giãn',
+        rssUrl: 'https://vnexpress.net/rss/thu-gian.rss',
+    },
+    {
+        id: 'entertainment',
+        name: 'Giải trí',
+        rssUrl: 'https://vnexpress.net/rss/giai-tri.rss',
+    },
+    {
+        id: 'science-technology',
+        name: 'Khoa học công nghệ',
+        rssUrl: 'https://vnexpress.net/rss/khoa-hoc-cong-nghe.rss',
+    },
+    {
+        id: 'most-viewed',
+        name: 'Tin xem nhiều',
+        rssUrl: 'https://vnexpress.net/rss/tin-xem-nhieu.rss',
+    },
+    {
+        id: 'current-affairs',
+        name: 'Thời sự',
+        rssUrl: 'https://vnexpress.net/rss/thoi-su.rss',
+    },
+    {
+        id: 'business',
+        name: 'Kinh doanh',
+        rssUrl: 'https://vnexpress.net/rss/kinh-doanh.rss',
+    },
+    {
+        id: 'education',
+        name: 'Giáo dục',
+        rssUrl: 'https://vnexpress.net/rss/giao-duc.rss',
+    },
+    {
+        id: 'real-estate',
+        name: 'Bất động sản',
+        rssUrl: 'https://vnexpress.net/rss/bat-dong-san.rss',
+    },
+    {
+        id: 'life',
+        name: 'Đời sống',
+        rssUrl: 'https://vnexpress.net/rss/gia-dinh.rss',
+    },
+    {
+        id: 'opinion',
+        name: 'Ý kiến',
+        rssUrl: 'https://vnexpress.net/rss/y-kien.rss',
+    },
+    {
+        id: 'travel',
+        name: 'Du lịch',
+        rssUrl: 'https://vnexpress.net/rss/du-lich.rss',
+    },
+    {
+        id: 'health',
+        name: 'Sức khỏe',
+        rssUrl: 'https://vnexpress.net/rss/suc-khoe.rss',
+    },
+    {
+        id: 'vehicles',
+        name: 'Xe',
+        rssUrl: 'https://vnexpress.net/rss/oto-xe-may.rss',
+    },
+    {
+        id: 'law',
+        name: 'Pháp luật',
+        rssUrl: 'https://vnexpress.net/rss/phap-luat.rss',
+    },
+    {
+        id: 'perspectives',
+        name: 'Góc nhìn',
+        rssUrl: 'https://vnexpress.net/rss/goc-nhin.rss',
+    },
 ];
 
 module.exports = categories;
